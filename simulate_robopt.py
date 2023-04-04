3710b2fc1fbdf343d6e51511bd5aef9740f29f27
Aart Stuurman <astuurman@hotmail.com>
2023-04-04 16:36:51 +0200

    rerun for rtgae

--- simulate_robopt.py
+++ simulate_robopt.py
@@ -8,6 +8,12 @@ import robot_optimization.rtgae.model as rmodel
 from revolve2.core.database import open_database_sqlite
 from sqlalchemy.orm import Session
 from evaluator import Evaluator
+from select_representations import Measure
+import pickle
+from rtgae.recursive_tree_grammar_auto_encoder import TreeGrammarAutoEncoder
+from rtgae import tree_grammar
+import torch
+from robot_rgt import make_body_rgt
 
 
 def load_robot_bench(run: int, optrun: int) -> ModularRobot:
@@ -29,8 +35,41 @@ def load_robot_bench(run: int, optrun: int) -> ModularRobot:
         return best_individual_last_gen.genotype.develop()
 
 
-def load_robot_rtgae(run: int, optrun: int) -> ModularRobot:
-    raise NotImplementedError()
+def load_body_model(
+    run: int, t_dim: int, r_dim: int, grammar: tree_grammar.TreeGrammar
+) -> TreeGrammarAutoEncoder:
+    model = TreeGrammarAutoEncoder(grammar, dim=t_dim, dim_vae=r_dim)
+    model.load_state_dict(
+        torch.load(config.TRAIN_OUT(run=run, t_dim=t_dim, r_dim=r_dim))
+    )
+    return model
+
+
+def load_robot_rtgae(run: int, optrun: int, bestorworst: bool) -> ModularRobot:
+    with open(config.SREP_OUT(run), "rb") as f:
+        selection: Measure = pickle.load(f)["best" if bestorworst else "worst"]
+    t_dim = selection.t_dim
+    r_dim = selection.r_dim
+
+    grammar = make_body_rgt()
+    body_model = load_body_model(run=run, t_dim=t_dim, r_dim=r_dim, grammar=grammar)
+
+    dbengine = open_database_sqlite(config.OPTRTGAE_OUT(run, optrun, bestorworst))
+    with Session(dbengine) as ses:
+        best_individual_last_gen = ses.scalar(
+            select(rmodel.Individual)
+            .join(rmodel.Population)
+            .join(rmodel.Generation)
+            .order_by(
+                rmodel.Generation.generation_index.desc(),
+                rmodel.Individual.fitness.desc(),
+            )
+            .limit(1)
+        )
+        assert best_individual_last_gen is not None
+
+        print(f"Fitness from database: {best_individual_last_gen.fitness}")
+        return best_individual_last_gen.genotype.develop(body_model)
 
 
 def main() -> None:
